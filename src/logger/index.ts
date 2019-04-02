621f8114a66a6e0f4c59167b5436fdab0403e32d
ahribori <ahribori@gmail.com>
2019-04-03 00:24:14 +0900

    Logger를 daily rotate하게 설정

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,5 +1,7 @@
+import * as DailyRotateFile from 'winston-daily-rotate-file';
 import * as winston from 'winston';
 import * as path from 'path';
+import * as dateFns from 'date-fns';
 
 interface ILogger {
   error: (message: string) => void;
@@ -8,37 +10,79 @@ interface ILogger {
   debug: (message: string) => void;
 }
 
+const logFormat = winston.format.printf(info => {
+  const { timestamp, level, message } = info;
+  return `${new Date(timestamp).toLocaleString()} ${level}: ${message}`;
+});
+
 const alignedWithColorsAndTime = winston.format.combine(
   winston.format.colorize(),
   winston.format.timestamp(),
   winston.format.printf(info => {
     const { timestamp, level, message, ...args } = info;
-    const ts = timestamp.slice(0, 19).replace('T', ' ');
-    return `${ts} [${level}]: ${message} ${
+    const time = dateFns.format(timestamp, 'YYYY-MM-DD HH:mm:ss');
+    return `${time} [${level}]: ${message} ${
       Object.keys(args).length ? JSON.stringify(args, null, 2) : ''
     }`;
   }),
 );
 
-const systemLog = winston.createLogger({
+const dailyRotateCombineTransport = new DailyRotateFile({
+  level: 'info',
+  filename: '%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '7d',
+  dirname: './log',
+  format: winston.format.combine(
+    winston.format.splat(),
+    winston.format.timestamp(),
+    logFormat,
+  ),
+});
+
+const dailyRotateErrorTransport = new DailyRotateFile({
+  level: 'error',
+  filename: '%DATE%.error.log',
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '7d',
+  dirname: './log',
+  format: winston.format.combine(
+    winston.format.splat(),
+    winston.format.timestamp(),
+    logFormat,
+  ),
+});
+
+const consoleTransport = new winston.transports.Console({
   format: alignedWithColorsAndTime,
-  transports: [new winston.transports.Console({ level: 'info' })],
+});
+
+const logger = winston.createLogger({
+  transports: [
+    dailyRotateCombineTransport,
+    dailyRotateErrorTransport,
+    consoleTransport,
+  ],
 });
 
 const getLogger = (fileName: string): ILogger => {
   const baseName = path.basename(fileName);
   return {
     error: (message: string) => {
-      systemLog.error(`(${baseName}) ${message}`);
+      logger.error(`(${baseName}) ${message}`);
     },
     warn: (message: string) => {
-      systemLog.warn(`(${baseName}) ${message}`);
+      logger.warn(`(${baseName}) ${message}`);
     },
     info: (message: string) => {
-      systemLog.info(`(${baseName}) ${message}`);
+      logger.info(`(${baseName}) ${message}`);
     },
     debug: (message: string) => {
-      systemLog.debug(`(${baseName}) ${message}`);
+      logger.debug(`(${baseName}) ${message}`);
     },
   };
 };
